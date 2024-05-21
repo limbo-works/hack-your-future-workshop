a83a36969d6d71f4ff0de1814e56c8b988e3ef24
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2024-05-21 14:05:42 +0200

    feat: implemented follow cursor example

--- 03-mouse/scripts.js
+++ 03-mouse/scripts.js
@@ -1,58 +1,11 @@
-let xActual = 0;
-let yActual = 0;
-let xTarget = 0;
-let yTarget = 0;
-let last = 0;
-
-/**
- * https://en.wikipedia.org/wiki/Linear_interpolation
- */
-function lerp(v0, v1, t) {
-  return (1 - t) * v0 + t * v1;
-}
-
-/**
- * Animation loop, see example: ...
- */
-function animate(now) {
-  window.requestAnimationFrame(animate);
-
-  /**
-   * Calculates time, since we last updated.
-   * Delta often just means difference.
-   * 16 = ~16ms per frame, when the frame rate is 60hz.
-   */
-  const delta = (now - last) / 16;
-  last = now;
-
-  /**
-   * Interpolates xActual and yActual towards xTarget and yTarget,
-   * taking the delta value into account.
-   *
-   * See example: ...
-   */
-  xActual = lerp(xActual, xTarget, 0.1 * delta);
-  yActual = lerp(yActual, yTarget, 0.1 * delta);
-
-  /**
-   * Sets xActual and yActual as CSS variables.
-   */
-  document.body.style.setProperty("--x", xActual);
-  document.body.style.setProperty("--y", yActual);
-}
-
-/**
- * Start the animation loop, once the content has loaded.
- */
-document.addEventListener("DOMContentLoaded", function () {
-  animate(performance.now());
-});
-
 /**
  * Detects changes in mouse position and saves them
- * in the variables x and y.
+ * in as css variables.
  */
 window.addEventListener("mousemove", function ({ clientX, clientY }) {
-  xTarget = clientX / window.innerWidth;
-  yTarget = clientY / window.innerHeight;
+  const x = clientX / window.innerWidth;
+  const y = clientY / window.innerHeight;
+
+  document.body.style.setProperty("--x", x);
+  document.body.style.setProperty("--y", y);
 });
