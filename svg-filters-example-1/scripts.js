9a060e9b4b01860669e6c7083897ef9deb38c926
Thomas Fjordside <48123373+tfjordside@users.noreply.github.com>
2024-05-22 08:34:14 +0200

    feat: css/svg filters

--- svg-filters-example-1/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("mousemove", function (e) {
-    const x = e.clientX;
-    const y = e.clientY;
-    document.documentElement.style.setProperty("--x", `${x}px`);
-    document.documentElement.style.setProperty("--y", `${y}px`);
-});
