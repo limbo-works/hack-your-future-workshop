030db3ae232e7200a05e6599cf238c8eea655122
Aske Lange <44546482+AskeLange@users.noreply.github.com>
2025-01-27 13:36:25 +0100

    feat: svg filters

--- 5-1-svg-filters/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("mousemove", function (e) {
-        const x = e.clientX;
-        const y = e.clientY;
-        document.documentElement.style.setProperty("--x", `${x}px`);
-        document.documentElement.style.setProperty("--y", `${y}px`);
-    });
-});
