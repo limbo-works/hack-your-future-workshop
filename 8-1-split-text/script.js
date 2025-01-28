f2145ccce07552c0b40cbfe8d31470b67fa29aee
toby044 <tobiasnolesen@gmail.com>
2025-01-28 08:55:16 +0100

    split

--- 8-1-split-text/script.js
+++ 8-1-split-text/script.js
@@ -1,11 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   const items = document.querySelectorAll(".target");
+  const splitWords = document.querySelectorAll(".target-words");
 
   items.forEach((item) => {
+    let index = 0;
+
     const text = item.textContent;
     item.innerHTML = text
-      .split("")
-      .map((char, index) => `<span style="--index:${index}">${char}</span>`)
-      .join("");
+      .split(" ")
+      .map((word) => {
+        return word
+          .split("")
+          .map((char) => `<span style="--index:${index++}">${char}</span>`)
+          .join("");
+      })
+      .join(" ");
+  });
+
+  splitWords.forEach((item) => {
+    let index = 0;
+
+    const text = item.textContent;
+    item.innerHTML = text
+      .split(" ")
+      .map((char) => `<span style="--index:${index++}">${char}</span>`)
+      .join(" ");
   });
 });
